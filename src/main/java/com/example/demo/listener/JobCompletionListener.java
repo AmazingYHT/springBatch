060bed03e253af3300a0eff38e318d00d332af7e
yonghongtao <yonghongtao@cnaidun.com>
2018-09-06 18:52:13 +0800

    new

--- src/main/java/com/example/demo/listener/JobCompletionListener.java
+++ src/main/java/com/example/demo/listener/JobCompletionListener.java
@@ -5,10 +5,12 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 
 public class JobCompletionListener extends JobExecutionListenerSupport {
-	
+
 	public void afterJob(JobExecution jobExecution) {
-		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {// 执行成功
 			System.out.println("BATCH JOB COMPLETED SUCCESSFULLY");
+		} else if (jobExecution.getStatus() == BatchStatus.FAILED) {// 失败
+			System.out.println("BATCH JOB COMPLETED FAILED");
 		}
 	}
 }
